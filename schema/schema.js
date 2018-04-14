910fdb45fc2b0ceea6c0c254671b1ce1b19eb4d7
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-04-13 20:56:52 -0500

    Updated schema and RootQuery type.

--- schema/schema.js
+++ schema/schema.js
@@ -1,19 +1,51 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 const {
   GraphQLInt,
   GraphQLObjectType,
   GraphQLString,
-} = ;
+  GraphQLSchema,
+} = require('graphql');;
+
+const users = [
+  {
+    id: '23',
+    firstName: 'Bill',
+    lastName: 'Clinton',
+    age: 47,
+  },
+  {
+    id: '24',
+    firstName: 'Tim',
+    lastName: 'Burton',
+    age: 20,
+  },
+]
 
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt },
-  }
+    lastName: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  },
 });
 
 const RootQuery = new GraphQLObjectType({
-  
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+        resolve(parentValue, args) {
+          return _.find(users, { id: args.id });
+        }
+      }
+    }
+});
+
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
