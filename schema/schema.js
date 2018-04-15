9f7a54e953a26d9737f0c5d0d2a8b460d785d194
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-04-14 23:06:11 -0500

    Added editUser mutation

--- schema/schema.js
+++ schema/schema.js
@@ -9,6 +9,8 @@ const {
   GraphQLNonNull,
 } = require('graphql');;
 
+const baseUrl = 'http://localhost:3000';
+
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
   fields: () => ({
@@ -18,7 +20,7 @@ const CompanyType = new GraphQLObjectType({
     users: {
       type: new GraphQLList(UserType),
       resolve(parentValue, args) {
-        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+        return axios.get(`${baseUrl}/companies/${parentValue.id}/users`)
         .then(res => res.data);
       }
     }
@@ -36,7 +38,7 @@ const UserType = new GraphQLObjectType({
     company: {
       type: CompanyType,
       resolve(parentValue, args) {
-        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+        return axios.get(`${baseUrl}/companies/${parentValue.companyId}`)
         .then(res => res.data);
       }
     }
@@ -50,7 +52,7 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return axios.get(`http://localhost:3000/users/${args.id}`)
+        return axios.get(`${baseUrl}/users/${args.id}`)
         .then(res => res.data);
       }
     },
@@ -72,6 +74,7 @@ const mutation = new GraphQLObjectType({
       type: UserType,
       args: {
         firstName: { type: new GraphQLNonNull(GraphQLString) },
+        lastName: { type: new GraphQLNonNull(GraphQLString) },
         age: { type: GraphQLInt },
         companyId: { type: GraphQLString }
       },
@@ -88,6 +91,20 @@ const mutation = new GraphQLObjectType({
       resolve(parentValue, { id }) {
         return axios.delete(`http://localhost:3000/users/${id}`)
           .then(res => res.data);
+      }
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString },
+      },
+      resolve(parentValue, args) {
+        return axios.patch(`${baseUrl}/users/${args.id}`, args)
+        .then(res => res.data);
       }
     }
   }
